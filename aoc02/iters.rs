d81883f120b6b5579a799ca0dc60aa6e0e2d9ad0
TTrebron <tr3bron@protonmail.com>
2025-12-03 01:18:18 +0100

    Add Day 2 Part 1 solution without lower bound checking

--- aoc02/iters.rs
+++ aoc02/iters.rs
@@ -1,3 +1,5 @@
+use std::ops::{Div, Range, Rem};
+
 pub struct RangesParser {
     input: String,
 }
@@ -36,18 +38,76 @@ impl Iterator for RangesParser {
     }
 }
 
+fn split_in_half(num: u64) -> (u32, u32, u32, u64, u64) {
+    // let digits_min = min == 0 ? 1 : floor(log10(min)) + 1
+    // let upper_half_digits_min = ceil(digits_min/2);
+    // let lower_half_digits_min = digits_min - upper_half_digits_min;
+    // let lower_half_min = min % 10^lower_half_digits_min;
+    // let upper_half_min = (min - lower_half_min)/10^(lower_half_digits_min)
+
+    let digits = num.checked_ilog10().unwrap_or_default() + 1;
+    let upper_half_digits = digits.div_ceil(2);
+    let lower_half_digits = digits - upper_half_digits;
+    let upper_half = num.div(u64::pow(10, lower_half_digits));
+    let lower_half = num.rem(u64::pow(10, lower_half_digits));
+    (
+        digits,
+        upper_half_digits,
+        lower_half_digits,
+        upper_half,
+        lower_half,
+    )
+}
+
+#[derive(Debug)]
 pub struct InvalidIdIterator {
     min: u64,
+    digits_min: u32,
+    upper_half_digits_min: u32,
+    lower_half_digits_min: u32,
+    upper_half_min: u64,
+    lower_half_min: u64,
     max: u64,
-    last: u64,
+    digits_max: u32,
+    upper_half_digits_max: u32,
+    lower_half_digits_max: u32,
+    upper_half_max: u64,
+    lower_half_max: u64,
+    last_pattern: u64,
+    digits_last_pattern: u32,
 }
 
 impl InvalidIdIterator {
     pub fn new(min: u64, max: u64) -> InvalidIdIterator {
+        let (
+            digits_min,
+            upper_half_digits_min,
+            lower_half_digits_min,
+            upper_half_min,
+            lower_half_min,
+        ) = split_in_half(min);
+        let (
+            digits_max,
+            upper_half_digits_max,
+            lower_half_digits_max,
+            upper_half_max,
+            lower_half_max,
+        ) = split_in_half(max);
         InvalidIdIterator {
             min: min,
+            digits_min: digits_min,
+            upper_half_digits_min: upper_half_digits_min,
+            lower_half_digits_min: lower_half_digits_min,
+            upper_half_min: upper_half_min,
+            lower_half_min: lower_half_min,
             max: max,
-            last: 0,
+            digits_max: digits_max,
+            upper_half_digits_max: upper_half_digits_max,
+            lower_half_digits_max: lower_half_digits_max,
+            upper_half_max: upper_half_max,
+            lower_half_max: lower_half_max,
+            last_pattern: 0,
+            digits_last_pattern: 0,
         }
     }
 }
@@ -56,6 +116,16 @@ impl Iterator for InvalidIdIterator {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        let current_pattern = match self.last_pattern {
+            0 => self.upper_half_min,
+            val => val + 1,
+        };
+        self.digits_last_pattern = current_pattern.checked_ilog10().unwrap_or_default() + 1;
+
+        let num = current_pattern + current_pattern * u64::pow(10, self.digits_last_pattern);
+
+        self.last_pattern = current_pattern;
+
+        if num > self.max { None } else { Some(num) }
     }
 }
