424a5f9b2c06f3b3b4c06b8fcc5ebfbfff058b63
TTrebron <tr3bron@protonmail.com>
2025-12-05 00:06:03 +0100

    Refactor Day 2, add split position parameter to InvalidIdIterator

--- aoc02/iters.rs
+++ aoc02/iters.rs
@@ -69,6 +69,17 @@ fn split_at(num: u64, index: u32) -> (u32, u32, u32, u64, u64) {
     )
 }
 
+fn split_at_get_upper(num: u64, index: u32) -> u64 {
+    if index == 0 {
+        return num;
+    }
+    let digits = num.checked_ilog10().unwrap_or_default() + 1;
+    let lower_half_digits = digits - index;
+    let upper_half = num.div(u64::pow(10, lower_half_digits));
+
+    upper_half
+}
+
 #[derive(Debug)]
 pub struct InvalidIdIterator {
     min: u64,
@@ -85,24 +96,25 @@ pub struct InvalidIdIterator {
     lower_half_max: u64,
     last_pattern: u64,
     digits_last_pattern: u32,
+    prefix_digits_limit: u32,
 }
 
 impl InvalidIdIterator {
-    pub fn new(min: u64, max: u64) -> InvalidIdIterator {
+    pub fn new(min: u64, max: u64, prefix_digits_limit: Option<u32>) -> InvalidIdIterator {
         let (
             digits_min,
             upper_half_digits_min,
             lower_half_digits_min,
             upper_half_min,
             lower_half_min,
-        ) = split_at(min, get_half_point(min));
+        ) = split_at(min, prefix_digits_limit.unwrap_or(get_half_point(min)));
         let (
             digits_max,
             upper_half_digits_max,
             lower_half_digits_max,
             upper_half_max,
             lower_half_max,
-        ) = split_at(max, get_half_point(max));
+        ) = split_at(max, prefix_digits_limit.unwrap_or(get_half_point(max)));
         InvalidIdIterator {
             min: min,
             digits_min: digits_min,
@@ -118,6 +130,7 @@ impl InvalidIdIterator {
             lower_half_max: lower_half_max,
             last_pattern: 0,
             digits_last_pattern: 0,
+            prefix_digits_limit: prefix_digits_limit.unwrap_or(0),
         }
     }
 }
@@ -129,8 +142,15 @@ impl Iterator for InvalidIdIterator {
         let current_pattern = match self.last_pattern {
             0 => {
                 if self.upper_half_digits_min * 2 < self.digits_min {
-                    u64::pow(10, std::cmp::max(self.upper_half_digits_min, 0)) // if we have 5(42) as min, start at the next new digit: 10(00)
-                } else if self.lower_half_min > self.upper_half_min {
+                    // next generated number would be < min
+                    if self.prefix_digits_limit == 0 {
+                        u64::pow(10, std::cmp::max(self.upper_half_digits_min, 0)) // if we have 5(42) as min, start at the next new digit: 10(00)
+                    } else {
+                        return None; // fixed prefix, no number would be larger than min
+                    }
+                } else if split_at_get_upper(self.lower_half_min, self.prefix_digits_limit)
+                    > self.upper_half_min
+                {
                     self.upper_half_min + 1
                 } else {
                     self.upper_half_min
@@ -138,10 +158,15 @@ impl Iterator for InvalidIdIterator {
             }
             val => val + 1,
         };
-        self.digits_last_pattern = current_pattern.checked_ilog10().unwrap_or_default() + 1;
 
-        let num = current_pattern + current_pattern * u64::pow(10, self.digits_last_pattern);
+        let digits_current_pattern = current_pattern.checked_ilog10().unwrap_or_default() + 1;
+        if self.prefix_digits_limit > 0 && digits_current_pattern > self.prefix_digits_limit {
+            return None;
+        }
 
+        let num = current_pattern + current_pattern * u64::pow(10, digits_current_pattern);
+
+        self.digits_last_pattern = digits_current_pattern;
         self.last_pattern = current_pattern;
 
         if num > self.max { None } else { Some(num) }
